09f53c2f577eb1ebd0e6cabe15d576712a991c7a
Milad Zolfaghari <miladsmacbook@Milads-MacBook-Pro.local>
2020-07-15 08:05:12 -0700

    update settings

--- samarssite/settings.py
+++ samarssite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '8f0381585469e6f3a6bf1d47ff485f089484d941578fb024'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
